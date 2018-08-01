89db907fbac1ef94c92fd4764d94c8308c976aeb
Beau Baker <beaubaker1027@gmail.com>
2018-08-01 14:14:56 -0500

    cleaning

--- main.js
+++ main.js
@@ -2,9 +2,6 @@ const {app, Tray, Menu} = require('electron');
 const path = require('path');
 const url = require('url');
 
-let win;
-const index = `file://${__dirname}/index.html`
-
 function createTray() {
   tray = new Tray(path.join(__dirname, '/Img/casette.png'));
   const contextMenu = Menu.buildFromTemplate([
@@ -19,17 +16,4 @@ function createTray() {
   tray.setContextMenu(contextMenu);
 
 }
-app.on('ready', createTray)
-
-
-/*app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
-})
-
-app.on('activate', () => {
-  if(win === null) {
-    createWindow();
-  }
-})*/
+app.on('ready', createTray);
